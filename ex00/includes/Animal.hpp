dd4763fd85fdf856d01f200d4a844e05262d0b36
João <jocolognesi@gmail.com>
2025-08-27 11:25:55 -0300

    ex00

--- ex00/includes/Animal.hpp
+++ ex00/includes/Animal.hpp
@@ -11,7 +11,7 @@ public:
 	Animal();
 	Animal(const Animal& other);
 	Animal& operator=(const Animal& other);
-	~Animal();
+	virtual ~Animal();
 	std::string getType() const;
 	virtual void makeSound() const;
 };
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,10 +1,34 @@
 #include "../includes/Animal.hpp"
 #include "../includes/Dog.hpp"
+#include "../includes/Cat.hpp"
+#include "../includes/WrongAnimal.hpp"
+#include "../includes/WrongCat.hpp"
 #include <iostream>
 
 int main(){
-	Dog a;
-	std::cout << a.getType() << std::endl;
-	a.makeSound();
-	return 0;
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << j->getType() << std::endl;
+	std::cout << i->getType() << std::endl;
+	i->makeSound();
+	j->makeSound();
+	meta->makeSound();
+	delete meta;
+	delete i;
+	delete j;
+
+	//WRONG ANIMAL
+	std::cout << std::endl;
+	std::cout << "-- WrongAnimal test --" << std::endl;
+	WrongAnimal* wrong = new WrongAnimal();
+	WrongAnimal* wrongcat = new WrongCat();
+	std::cout << wrong->getType() <<std::endl;
+	std::cout << wrongcat->getType() <<std::endl;
+	wrong->makeSound();
+	wrongcat->makeSound();
+	delete wrong;
+	delete wrongcat;
+	return(0);
 }
